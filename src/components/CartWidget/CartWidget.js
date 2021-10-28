59b5cd9d862cda40ea62079b44bb60ee5bbb71b0
gabiten03 <gabiten@gmail.com>
2021-10-28 00:03:56 -0300

    cartwidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,28 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { IconButton } from '@chakra-ui/button'
 
 import { BiShoppingBag } from 'react-icons/bi'
-import { Link } from '@chakra-ui/layout'
+import { HStack, Link, Text } from '@chakra-ui/layout'
+import { CartContext } from '../../CartContext';
+//for each element in cartproduct    count the quantity of each product and sum the total of products   
+
 
 function CartWidget() {
+    let total = 0
+    const [cartproduct, setCartProduct, addProduct, removeProduct, clear, isInCart] = useContext(CartContext);
+    console.log(cartproduct)
+    if (cartproduct !== undefined) {
+        cartproduct.forEach((item) => {
+            console.log(item)
+            total += item.quantity
+
+        })
+    }
+    console.log(total)
+
     return (
         <>
             <Link href='/cart' >
-                <IconButton icon={<BiShoppingBag />} isRound='true'
-                    display={{ base: 'none', md: 'inline-flex' }}
-                    fontSize={'sm'}
-                    fontWeight={600}
-                    color={'white'}
-                    bg={'pink.400'}
-                    href={'#'}
-                    _hover={{
-                        bg: 'pink.300',
-                    }}>
+                <HStack >
+                    <IconButton icon={<BiShoppingBag />} isRound='true'
+                        display={{ base: 'none', md: 'inline-flex' }}
+                        fontSize={'sm'}
+                        fontWeight={600}
+                        color={'white'}
+                        bg={'pink.400'}
+                        href={'#'}
+                        _hover={{
+                            bg: 'pink.300',
+                        }}>
 
-                </IconButton>
+                    </IconButton>
+                    <Text display={{ base: 'none', md: 'inline-flex' }} marginLeft={12} zIndex='999' color='black'> {total}
+                    </Text>
+                </HStack>
+
             </Link>
         </>
     )
-}
 
+}
 export default CartWidget
